ddbf37c0dbba0a20c76ca37fe63684690d4255d4
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2019-12-13 15:13:03 -0500

    Finished User sheet page

--- reactjs/3headeddragon/src/components/sheets/index.js
+++ reactjs/3headeddragon/src/components/sheets/index.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, CardGroup } from 'reactstrap';
+import { FaTrash } from 'react-icons/fa';
+import styles from './styles.module.css';
 
 class Sheets extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={styles.sheets}>
+                <Container>
+                    <div className={styles.heading}>
+                        <h2>Characters</h2>
+                        <Button>Add</Button>
+                    </div>
+                    <Row xs="3" className={styles.container}>
+                        <Col>
+                            <Card className={styles.card}>
+                                <FaTrash className={styles.icon}/>
+                                <CardImg top src="henry.jpg" alt="Character Image" height="280px"/>
+                                <CardBody>
+                                <h2><CardTitle>Sir Henry</CardTitle></h2>
+                                <CardSubtitle>Dungeons Of Dread</CardSubtitle>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className={styles.card}>
+                                <FaTrash className={styles.icon}/>
+                                <CardImg top src="henry.jpg" alt="Character Image" height="280px"/>
+                                <CardBody>
+                                <h2><CardTitle>Sir Henry</CardTitle></h2>
+                                <CardSubtitle>Dungeons Of Dread</CardSubtitle>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className={styles.card}>
+                                <FaTrash className={styles.icon}/>
+                                <CardImg top src="henry.jpg" alt="Character Image" height="280px"/>
+                                <CardBody>
+                                <h2><CardTitle>Sir Henry</CardTitle></h2>
+                                <CardSubtitle>Dungeons Of Dread</CardSubtitle>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className={styles.card}>
+                                <FaTrash className={styles.icon}/>
+                                <CardImg top src="henry.jpg" alt="Character Image" height="280px"/>
+                                <CardBody>
+                                <h2><CardTitle>Sir Henry</CardTitle></h2>
+                                <CardSubtitle>Dungeons Of Dread</CardSubtitle>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
