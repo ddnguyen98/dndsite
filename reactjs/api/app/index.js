891b199a738789eccd261369da1e66331816687e
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2019-12-13 18:48:42 -0500

    Heroku setup

--- reactjs/api/app/index.js
+++ reactjs/api/app/index.js
@@ -1,12 +1,14 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, '../../3headeddragon/build')));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static(path.join(__dirname, '../../3headeddragon/build')));
 
-    app.get('*', function(req,res){
-        res.sendFile(path.join(__dirname, '../../3headeddragon/build', 'index.html'))
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../3headeddragon/build', 'index.html'));
+  });
 }
 
 module.exports = app;
