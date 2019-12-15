3f0da75a05c9c05479eefe44f6171334783cd808
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2019-12-15 09:58:24 -0500

    Added plugin.

--- reactjs/src/components/character/index.js
+++ reactjs/src/components/character/index.js
@@ -9,6 +9,7 @@ import {
   Col,
   Row,
 } from 'reactstrap';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { FaEye } from 'react-icons/fa';
 import styles from './styles.module.css';
 
@@ -171,13 +172,253 @@ class index extends Component {
                 </Row>
                 <Row xs="3">
                   <Col>
-                    <div>Content</div>
+                    <div>
+                      {/* HP */}
+                      <div>
+                        <h2>Hit Points</h2>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        /
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                      </div>
+                      {/* Resistances */}
+                      <div>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            type="textarea"
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                      </div>
+                      {/* Sped + inititive */}
+                      <div>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <div>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                        </div>
+                      </div>
+                    </div>
                   </Col>
                   <Col>
-                    <div>Content</div>
+                    <div>
+                      {/* Armor */}
+                      <div>
+                        <div>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <div>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                          </div>
+                        </div>
+                      </div>
+                      {/* Saving Throws */}
+                      <div>
+                        <h2>Saving Throws</h2>
+                        <div>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <div>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                            <FormGroup>
+                              <Label for="input">Input</Label>
+                              <Input
+                                name="email"
+                                id="input"
+                                placeholder="with a placeholder"
+                              />
+                            </FormGroup>
+                          </div>
+                        </div>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <div>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                        </div>
+                        <FormGroup>
+                          <Label for="input">Input</Label>
+                          <Input
+                            name="email"
+                            id="input"
+                            placeholder="with a placeholder"
+                          />
+                        </FormGroup>
+                        <div>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="input">Input</Label>
+                            <Input
+                              name="email"
+                              id="input"
+                              placeholder="with a placeholder"
+                            />
+                          </FormGroup>
+                        </div>
+                      </div>
+                    </div>
                   </Col>
                   <Col>
-                    <div>Content</div>
+                    <div>
+                      {/* Attack Bonus */}
+                      <div>Content</div>
+                      {/* weight */}
+                      <div>Content</div>
+                    </div>
                   </Col>
                 </Row>
               </Col>
